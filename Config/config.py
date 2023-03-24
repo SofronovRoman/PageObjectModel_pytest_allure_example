dab16bb7b432e3d20745755e5d576ebf1e0c34f2
Sofronov <privatcard@mail.ru>
2023-03-24 16:09:59 +0300

    Initial commit

--- Config/config.py
+++ Config/config.py
@@ -1,9 +1,8 @@
 class TestData:
-    CHROMEDRIVER_PATH = r'D:\programs\Python\chromedriver_win32\chromedriver.exe'
+    CHROMEDRIVER_PATH = r'path_to_chromedriver'
     BASE_URL = 'https://instagram.com'
     USERNAME = 'username'
     PASSWORD = 'password'
-    COOKIE = str([{'domain': '.instagram.com', 'httpOnly': True, 'name': 'rur', 'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': '"NCG\\05458630963411\\0541709893606:01f78a383ee85373cc36bd7c376a4d751c1ae87b86a6edff8841f5cf2174671c95aed398"'}, {'domain': '.instagram.com', 'expiry': 1709893601, 'httpOnly': True, 'name': 'sessionid', 'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': '58630963411%3ACKfnQcQE4BF8yq%3A18%3AAYcVB8IwUCbhVL-1oA7u9P4XFmVMHLdiboVpX9EvwA'}, {'domain': '.instagram.com', 'expiry': 1709807204, 'httpOnly': False, 'name': 'csrftoken', 'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': '0XO54YWhfGRDV3LkFfIp9DU6zeSAqQYG'}, {'domain': '.instagram.com', 'expiry': 1712917491, 'httpOnly': True, 'name': 'datr', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '8bMJZIMCq1HSGkQI-QkVFHwd'}, {'domain': '.instagram.com', 'expiry': 1686133604, 'httpOnly': False, 'name': 'ds_user_id', 'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': '58630963411'}, {'domain': '.instagram.com', 'expiry': 1709893491, 'httpOnly': True, 'name': 'ig_did', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '4ADA6A57-1C53-425F-A94A-50BE1ADE9670'}, {'domain': '.instagram.com', 'expiry': 1709893491, 'httpOnly': False, 'name': 'ig_nrcb', 'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': '1'}, {'domain': '.instagram.com', 'expiry': 1712917491, 'httpOnly': False, 'name': 'mid', 'path': '/', 'sameSite': 'Lax', 'secure': True, 'value': 'ZAmz9AALAAF3M7Q2ant9l_OvtOoR'}, {'domain': '.instagram.com', 'expiry': 1678962403, 'httpOnly': False, 'name': 'dpr', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '1.25'}])
     START_PAGE_TITLE = 'Instagram'
     GOOGLE_PLAY_ICON_LINK = 'https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png'
     MICROSOFT_ICON_LINK = 'https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png'
